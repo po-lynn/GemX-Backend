56a43f821c6a4dfc15afea783964ef654d48f415
polynn <polynn@global-connect.asia>
2026-09-12 09:35:33 +0630

    queue fixt

--- drizzle/migrations/0091_terms_conditions_section.sql
+++
@@ -1,2 +0,0 @@
--- App Content: Terms & Conditions section (BlockNote multilang JSON in jsonb)
-ALTER TYPE "public"."app_content_section_name" ADD VALUE IF NOT EXISTS 'terms_conditions';
--- drizzle/migrations/0094_windy_wolfsbane.sql
+++
@@ -1,2 +0,0 @@
-ALTER TYPE "public"."app_content_section_name" ADD VALUE 'buying_guide';--> statement-breakpoint
-ALTER TYPE "public"."app_content_section_name" ADD VALUE 'selling_guide';
